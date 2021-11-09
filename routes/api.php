cd703fa632752fadae988dd624b0692ff971e03a
abir bikal <tipu.dany@gmail.com>
2021-11-09 19:56:47 +0600

    Change Route Group

--- routes/api.php
+++ routes/api.php
@@ -2,19 +2,21 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
-use Illuminate\Foundation\Auth\EmailVerificationRequest;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => 'api',], function ($router) {
 
     Route::post('login', [AuthController::class, 'login'])->middleware('verified');
-    Route::post('logout', [AuthController::class, 'logout']);
     Route::post('refresh', [AuthController::class, 'refresh']);
-    Route::get('me', [AuthController::class, 'me']);
 
-    Route::resource('users',UserController::class)->middleware('auth');
-    Route::post('users/change-password/{id}',[UserController::class,'changePassword']);
+    Route::group(['middleware' => 'auth','verified'],function ($router){
+
+        Route::post('logout', [AuthController::class, 'logout']);
+        Route::get('me', [AuthController::class, 'me']);
+
+        Route::resource('users',UserController::class);
+        Route::post('users/change-password/{id}',[UserController::class,'changePassword']);
+    });
 
     Route::get('/email/verify/{id}/{token}',[AuthController::class, 'verify'] );
 });
