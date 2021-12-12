9d090cb52677a08582073c07b870f09ecbace365
Tipu <tipu.dany@gmail.com>
2021-12-13 01:35:13 +0600

    change user db

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->tinyInteger('email_verified')->default(0);
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('email_verification_token');
+            $table->string('email_verification_token')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+
+    // write some code about service container and service providers.
+
+    //$this->app()->bind()      //use for bind new service to the Service Container.
+
+    //$this->app()->make()      // use for make instance for this Services to use anywhere.
+
     return view('welcome');
 });
